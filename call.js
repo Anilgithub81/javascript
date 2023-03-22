14ff13d66a1f1b270ee2bc546472c6bfe51d9a5b
Anilgithub81 <127197944+Anilgithub81@users.noreply.github.com>
2023-03-22 21:25:58 +0530

    Delete call.js

--- call.js
+++
@@ -1,8 +0,0 @@
-function Emp(id,name) {  
-    this.id = id;   
-    this.name = name;  
-  }  
-  function PermanentEmp(id,name) {  
-   Emp.call(this,id,name);  
-  }  
-  console.log(new PermanentEmp(1 , "anil ").name); 
