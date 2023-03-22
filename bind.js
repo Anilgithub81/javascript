051444731f8c498c17ceeed78da3596f0c0bdc7e
Anilgithub81 <127197944+Anilgithub81@users.noreply.github.com>
2023-03-22 21:25:39 +0530

    Delete bind.js

--- bind.js
+++
@@ -1,9 +0,0 @@
-var person = {  
-    name: "Javascript",  
-    getName: function() {  
-      return this.name;  
-    }  
-  }  
-  var GetName = person.getName;  
-  var Get_Name = GetName.bind(website);  
-  console.log(GetName());  
